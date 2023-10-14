7f2415789d9d6e652342de9647d09a6e4bd709bc
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-10-14 19:27:51 +0100

    Add Categories to todo

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const CreateTodo = () => {
-  return <div></div>;
+  const [category, setCategory] = useState("");
+
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Create Task</h1>
+      <div className={styles.select}>
+        <label htmlFor="continent">Filter by Continents</label>
+        <select id="continents" name="continents" onChange={handleCategory}>
+          <option value="africa">Chores</option>
+          <option value="asia">Work</option>
+          <option value="europe">Gym</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTodo;
